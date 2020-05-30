6b42eed526292245d3b6933807bb0454555c8a0d
JaviFerri7 <jferri45@gmail.com>
2020-05-30 10:40:37 -0300

    2 files modified, 3 added

---
+++ src/app/app-multas/app-multas.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-app-multas',
+  templateUrl: './app-multas.component.html',
+  styleUrls: ['./app-multas.component.css']
+})
+export class AppMultasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,12 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { UserScreenComponent } from './user-screen/user-screen.component';
 import { SrtComponent } from './srt/srt.component';
-import { NewsComponent } from './news/news.component';
+import { AppMultasComponent } from './app-multas/app-multas.component';
+
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, UserScreenComponent, SrtComponent, NewsComponent ],
+  declarations: [ AppComponent, HelloComponent, UserScreenComponent, SrtComponent, AppMultasComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
