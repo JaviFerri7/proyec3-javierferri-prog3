b98180651d198004d45816e34c6ec214df71c211
JaviFerri7 <jferri45@gmail.com>
2020-05-31 20:50:02 -0300

    2 files modified

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import{RouterModule, Routes}from '@angular/router';
 
 import { AppMultasComponent } from "./app-multas/app-multas.component";
 import { SrtComponent } from "./srt/srt.component";
-
+import { UserScreenComponent } from "./user-screen/user-screen.component";
+ 
 const app_routes: Routes=[
   {path:'app-app-multas', component: AppMultasComponent},
-  {path:'app-srt', component: SrtComponent}
+  {path:'app-srt', component: SrtComponent},
+  {path:'app-user-screen', component: UserScreenComponent}
 ];
 
 export const app_routing = RouterModule.forRoot(app_routes);
