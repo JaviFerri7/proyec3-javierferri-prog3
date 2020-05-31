e1957ea27873d6e55a9d5af342a2b4b4361ef9f3
JaviFerri7 <jferri45@gmail.com>
2020-05-31 20:34:50 -0300

    1 file modified

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,11 @@
 import{RouterModule, Routes}from '@angular/router';
 
-import { multasComponent } from ".//app-multas.component.html";
-import { srtComponent } from "./srt/srt.component.html";
+import { MultasComponent } from "./app-multas/app-multas.component";
+import { SemperReinventComponent } from "./srt/srt.component";
 
 const app_routes: Routes=[
-  {path:'app-app-multas', component: multasComponent},
-  {path:'app-srt', component: srtComponent}
+  {path:'app-app-multas', component: MultasComponent},
+  {path:'app-srt', component: SemperReinventComponent}
 ];
 
 export const app_routing = RouterModule.forRoot(app_routes);
